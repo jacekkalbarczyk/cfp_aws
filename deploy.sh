e4e46124b8a1b8c0529f8c7a3bf4cabc80450095
Jacek Kalbarczyk <kalbarczyk@gmail.com>
2020-06-22 21:23:51 +0200

    S8_____Deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,7 @@
 # build and push frontend
 pushd frontend
     yarn run build
-    aws s3 sync dist s3://cfp-v3-s3-bucket
-    # aws s3 sync frontend/dist/ s3://cfp-v3-s3-bucket
+    aws s3 sync --quiet dist s3://cfp-v3-s3-bucket
     # aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
 popd
 
