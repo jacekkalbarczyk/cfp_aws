248fa5348c2d22e9c537a7ccadebeaed932fdb71
Jacek Kalbarczyk <kalbarczyk@gmail.com>
2020-06-22 20:40:57 +0200

    S3___Deploy

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# build and push frontend
+pushd frontend
+    yarn run build
+    aws s3 sync dist s3://cfp-v3-s3-bucket
+    # aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
+popd
+
+# build and push backend
+pip install --quiet -r backend/requirements.txt --target packages
