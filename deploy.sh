b49d6590bf0e5d766d6e0afc53a4797bf6d24e33
Jacek Kalbarczyk <kalbarczyk@gmail.com>
2020-06-23 23:06:24 +0200

    S3 Booya

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # build and push frontend
-# pushd frontend
-#     npm run build
-#     aws s3 sync dist s3://cfp-v3-s3-bucket
-#     # aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
-# popd
-(cd frontend && npm     run       build)
-aws s3 sync dist s3://cfp-v3-s3-bucket
+pushd frontend
+    npm run build
+    aws s3 sync dist s3://cfp-v3-s3-bucket
+    # aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
+popd
+# (cd frontend && npm     run       build)
+# aws s3 sync dist s3://cfp-v3-s3-bucket
 
 # build and push backend
 pip install --quiet -r backend/requirements.txt --target packages
