fd403ddf1e4f8a4999b6ceb0f878f35979b0d676
Jacek Kalbarczyk <kalbarczyk@gmail.com>
2020-06-23 23:19:38 +0200

    S3 Booyakasha2

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 # build and push frontend
 pushd frontend
-    npm run build
+    yarn run build
     aws s3 sync dist s3://cfp-v3-s3-bucket
     # aws cloudfront create-invalidation --paths "/*" --distribution-id E6PZCV3N5WWJ8
 popd
